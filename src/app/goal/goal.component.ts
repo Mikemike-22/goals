8cac885c034673f40f495ead952141e4e1feb424
Michael Korir <mikor22@gmail.com>
2019-05-03 11:51:07 +0300

    create goal-details component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Goal} from '../goal'
 
 @Component({
   selector: 'app-goal',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalComponent implements OnInit {
 
+    goals = [
+        new Goal(1,'Watch Finding Nemo'),
+        new Goal(2,'Buy Cookies'),
+        new Goal(3, 'Get new Phone Case'),
+        new Goal(4, 'Get Dog Food'),
+        new Goal(5, 'Solve math homework'),
+        new Goal(6, 'Plot my world domination plan'),
+    ]
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
