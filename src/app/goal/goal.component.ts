81a82aa4c0c5a47d86669521f32679ad7d7b6158
Michael Korir <mikor22@gmail.com>
2019-05-03 15:05:24 +0300

    add functionality to add a goal in form

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -18,6 +18,13 @@ export class GoalComponent implements OnInit {
 
     ]
 
+    addNewGoal(goal){
+      let goalLength = this.goals.length;
+      goal.id=goalLength+1;
+      goal.completeDate = new Date(goal.completeDate)
+      this.goals.push(goal)
+    }
+
     deleteGoal(isComplete,index){
       if(isComplete){
         let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}`)
